198286b73fcc81a0ee43f0d25d4d8edfb8bae1c6
oharakogakuin <ohara@g.neec.ac.jp>
2023-04-20 12:25:18 +0900

    01_05.FBX SDK の組み込み

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -4,21 +4,22 @@
 #include "GameScene.h"
 #include "LightGroup.h"
 #include "ParticleManager.h"
-
+#include "fbxsdk.h"
 // Windowsアプリでのエントリーポイント(main関数)
-int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
+	FbxManager* fbxManager = FbxManager::Create();
 	// 汎用機能
 	WinApp* win = nullptr;
 	DirectXCommon* dxCommon = nullptr;
-	Input* input = nullptr;	
+	Input* input = nullptr;
 	Audio* audio = nullptr;
 	GameScene* gameScene = nullptr;
 
 	// ゲームウィンドウの作成
 	win = new WinApp();
 	win->CreateGameWindow();
-		
+
 	//DirectX初期化処理
 	dxCommon = new DirectXCommon();
 	dxCommon->Initialize(win);
@@ -50,12 +51,12 @@ int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int)
 	// ゲームシーンの初期化
 	gameScene = new GameScene();
 	gameScene->Initialize(dxCommon, input, audio);
-	
+
 	// メインループ
 	while (true)
 	{
 		// メッセージ処理
-		if (win->ProcessMessage()) {	break; }
+		if (win->ProcessMessage()) { break; }
 
 		// 入力関連の毎フレーム処理
 		input->Update();
