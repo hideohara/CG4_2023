21e719674796271bb8a07682c7c0bae3b97be5ad
oharakogakuin <ohara@g.neec.ac.jp>
2023-06-29 11:59:35 +0900

    03_03.レンダーテクスチャ生成

--- DirectXGame/2d/PostEffect.h
+++ DirectXGame/2d/PostEffect.h
@@ -14,5 +14,21 @@ public:
 	/// </summary>
 	/// <param name="cmdList">コマンドリスト</param>
 	void Draw(ID3D12GraphicsCommandList* cmdList);
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+private:
+	// Microsoft::WRL::を省略
+	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+
+	// テクスチャバッファ
+	ComPtr<ID3D12Resource> texBuff;
+	// SRV用デスクリプタヒープ
+	ComPtr<ID3D12DescriptorHeap> descHeapSRV;
+
+
 };
 
