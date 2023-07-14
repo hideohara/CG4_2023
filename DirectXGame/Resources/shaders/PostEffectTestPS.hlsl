34245f029d97726c3da62cb42dd4e98b152f5a51
oharakogakuin <ohara@g.neec.ac.jp>
2023-07-14 10:25:02 +0900

    03_08.マルチテクスチャ

--- DirectXGame/Resources/shaders/PostEffectTestPS.hlsl
+++ DirectXGame/Resources/shaders/PostEffectTestPS.hlsl
@@ -1,19 +1,32 @@
 #include "PostEffectTest.hlsli"
 
-Texture2D<float4> tex : register(t0); // 0番スロットに設定されたテクスチャ
+Texture2D<float4> tex0 : register(t0); // 0番スロットに設定されたテクスチャ
+Texture2D<float4> tex1 : register(t1); // 1番スロットに設定されたテクスチャ
+
 SamplerState smp : register(s0); // 0番スロットに設定されたサンプラー
 
 float4 main(VSOutput input) : SV_TARGET
 {
-    float4 texcolorTotal = 0;
-    for (float y = -1; y <= 1; y++)
-    {
-        for (float x = -1; x <= 1; x++)
-        {
-            texcolorTotal += tex.Sample(smp, input.uv + float2(x / 640, y / 360));
-        }
-    }
-    texcolorTotal /= 9;
+    //float4 texcolorTotal = 0;
+    //for (float y = -1; y <= 1; y++)
+    //{
+    //    for (float x = -1; x <= 1; x++)
+    //    {
+    //        texcolorTotal += tex.Sample(smp, input.uv + float2(x / 640, y / 360));
+    //    }
+    //}
+    //texcolorTotal /= 9;
 
-    return float4(texcolorTotal.rgb, 1);
+    //return float4(texcolorTotal.rgb, 1);
+    
+    float4 colortex0 = tex0.Sample(smp, input.uv);
+    float4 colortex1 = tex1.Sample(smp, input.uv);
+
+    float4 color = colortex0;
+    if (fmod(input.uv.y, 0.1f) < 0.05f)
+    {
+        color = colortex1;
+    }
+
+    return float4(color.rgb, 1);
 }
