1c14d3e3b0833d267e05d63625608a903dd51a7b
oharakogakuin <ohara@g.neec.ac.jp>
2023-07-20 11:48:44 +0900

    評価課題2

--- DirectXGame/Resources/shaders/PostEffectTestPS.hlsl
+++ DirectXGame/Resources/shaders/PostEffectTestPS.hlsl
@@ -7,22 +7,22 @@ SamplerState smp : register(s0); // 0番スロットに設定されたサンプ�
 
 float4 main(VSOutput input) : SV_TARGET
 {
-    //float4 texcolorTotal = 0;
-    //for (float y = -1; y <= 1; y++)
-    //{
-    //    for (float x = -1; x <= 1; x++)
-    //    {
-    //        texcolorTotal += tex.Sample(smp, input.uv + float2(x / 640, y / 360));
-    //    }
-    //}
-    //texcolorTotal /= 9;
+    float4 texcolorTotal = 0;
+    for (float y = -1; y <= 1; y++)
+    {
+        for (float x = -1; x <= 1; x++)
+        {
+            texcolorTotal += tex1.Sample(smp, input.uv + float2(x / 640, y / 360));
+        }
+    }
+    texcolorTotal /= 9;
 
     //return float4(texcolorTotal.rgb, 1);
     
     float4 colortex0 = tex0.Sample(smp, input.uv);
-    float4 colortex1 = tex1.Sample(smp, input.uv);
+    float4 colortex1 = texcolorTotal;
 
-    float4 color = colortex0;
+    float4 color = 1 - colortex0;
     if (fmod(input.uv.y, 0.1f) < 0.05f)
     {
         color = colortex1;
